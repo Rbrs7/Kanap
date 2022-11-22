cb9e8f64cef96549bcb08b0241778064b1edbe61
Rbrs7 <raphael.bressi@wanadoo.fr>
2022-11-22 10:08:36 +0100

    préparation affichage JS

--- js/script.js
+++ js/script.js
@@ -4,9 +4,24 @@ fetch("http://localhost:3000/api/products")
       return res.json();
     }
   })
-  .then(function (value) {
-    console.log(value);
+  .then(function (data) {
+    console.log(data);
   })
   .catch(function (err) {
     // Une erreur est survenue
   });
+
+/* let a = document.createElement("a");
+ a.href = "href='./product.html?id=42'";
+ let article = document.createElement("article");
+ a.append(article)
+ let image1 = document.createElement("img");
+ image1.setAttribute(
+  "src",
+  "`${kanap.imageUrl}`"
+ );
+ image1.setAttribute("alt", "`${kanap.altTxt}`")
+ article.append(image1);
+ etc... */
+
+ // peut-être enlever guillemets sur (`${kanap.imageUrl}`)
