b301d5df0df55a3bdab95b540b6376d94bcc35f5
Rbrs7 <raphael.bressi@wanadoo.fr>
2022-12-12 22:27:43 +0100

    function display data mise de côté

--- js/cart.js
+++ js/cart.js
@@ -1,35 +1,3 @@
-/*
-function display(data) {
-    let html = "";
-    data.map(function (kanap) {
-      html += `<article class="cart__item" data-id=${kanap._id} data-color=${kanap.colors}>
-      <div class="cart__item__img">
-        <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
-      </div>
-      <div class="cart__item__content">
-        <div class="cart__item__content__description">
-          <h2>${kanap.name}</h2>
-          <p>${kanap.colors}</p>
-          <p>${kanap.price}</p>
-        </div>
-        <div class="cart__item__content__settings">
-          <div class="cart__item__content__settings__quantity">
-            <p>Qté : </p>
-            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-          </div>
-          <div class="cart__item__content__settings__delete">
-            <p class="deleteItem">Supprimer</p>
-          </div>
-        </div>
-      </div>
-    </article>`;
-    });
-    console.log(html);
-    let itemsCart = document.getElementById("cart__items");
-    itemsCart.innerHTML = html;
-  }
-  */
-
 fetch("http://localhost:3000/api/products")
   .then(function (res) {
     if (res.ok) {
@@ -112,3 +80,34 @@ function getTotalPrice(){
   changeQuantity({id:"50"},-5) 
   getTotalPrice()
 */
+
+/*
+function display(data) {
+    let html = "";
+    data.map(function (kanap) {
+      html += `<article class="cart__item" data-id=${kanap._id} data-color=${kanap.colors}>
+      <div class="cart__item__img">
+        <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${kanap.name}</h2>
+          <p>${kanap.colors}</p>
+          <p>${kanap.price}</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+          </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
+          </div>
+        </div>
+      </div>
+    </article>`;
+    });
+    console.log(html);
+    let itemsCart = document.getElementById("cart__items");
+    itemsCart.innerHTML = html;
+  }
