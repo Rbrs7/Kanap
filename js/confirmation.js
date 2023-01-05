4a7f5358c8c94d7295fd20452afdbd6bdabc25fc
Rbrs7 <raphael.bressi@wanadoo.fr>
2023-01-05 18:27:03 +0100

    début de la page confirmation, élément du formulaire dans console.log -> ok

---
+++ js/confirmation.js
@@ -0,0 +1,23 @@
+const formElem = document.querySelector('form');
+formElem.addEventListener('submit', (event) => {
+  event.preventDefault();
+  new FormData(formElem);
+});
+
+formElem.onformdata = (event) => {
+  let data = event.formData;
+  for (var value of data.values()) {
+    console.log(value); 
+  }
+
+  const request = new XMLHttpRequest();
+  request.open("POST", "http://localhost:3000/api/products/order");
+  request.send[data];
+};
+
+let fd = new FormData();
+fd.append('test', 'test');
+let fdEv = new FormDataEvent('formdata', { formData: fd });
+for (let value of fdEv.formData.values()) {
+  console.log(value); 
+}
